2c126404ba229f3c636a80a3ef7b5bd761cec06a
flowu <guspostjo@student.gu.se>
2021-12-02 17:59:17 +0100

    Add solution for day 2. Change name of input file of day 1.

--- day1.py
+++ day1.py
@@ -5,7 +5,7 @@ def b(lines):
     return len([x for x in range(3,len(lines)+1) if sum(lines[x-2:x+1]) > sum(lines[x-3:x])])
 
 def main():
-    with open('C:/Users/Joel/Desktop/advent_of_code/input.txt','r') as file:
+    with open('C:/Users/Joel/Desktop/advent_of_code/input1.txt','r') as file:
         lines = [int(line) for line in file]
     
     print('a', a(lines))
